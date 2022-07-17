44f7343cb03259dee590aae07953b3a77043ff62
AkezhanR <rakishev-as@ext.sber-zvuk.com>
2022-07-17 21:37:57 +0600

    Task 3: serverless api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,13 @@
+type API_PROPS = {
+  product: string;
+  order: string;
+  import: string;
+  bff: string;
+  cart: string;
+}
 
-const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+const API_PATHS: API_PROPS = {
+  product: 'https://7f1t0szo2e.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
